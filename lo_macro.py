8633ab8763f0bf82f5ff880cb97813b56001fe72
tootiredone <kamalievkamil@gmail.com>
2020-11-28 02:58:02 +0300

    +fullname extraction +selected text updating

--- lo_macro.py
+++ lo_macro.py
@@ -2,26 +2,46 @@
 from __future__ import unicode_literals
 import urllib, json
 import urllib.request
+import re
 
 
-def my_first_macro():
+def OneToThree():
+    # preparation
+    desktop = XSCRIPTCONTEXT.getDesktop()
+    model = desktop.getCurrentComponent()
+    oSelected = model.getCurrentSelection()
     doc = XSCRIPTCONTEXT.getDocument()
     text = doc.getText()
 
-    tRange = text.End
-    tRange.String = "KEKEKEK"
+    # acquiring the selected text
+    oSel = oSelected.getByIndex(0)
+    selectedText = oSel.getString()
 
-    toSend = {'fname': "Artem Dmitriev", "text": tRange.String}
+    # obtaining all text
+    allText = text.Text.String
+
+    # splitting the text into lines
+    lines = allText.split('\n')
+
+    # obtaining full name
+    for line in lines:
+        if "Фамилия, имя, отчество" in line:
+            fname = line.replace('Фамилия, имя, отчество', ' ')
+            fname = re.sub('[^а-яА-Я]+', ' ', fname)
+            fname = ' '.join(fname.split(' '))
+            break
+
+    # sending the request to server
+    toSend = {'fname': fname, "text": selectedText}
     data = json.dumps(toSend).encode("utf8")
     url = 'http://localhost:8080/send'
     req = urllib.request.Request(url, data=data, headers={'content-type': 'application/json'})
 
+    # transforming response to json
     res = urllib.request.urlopen(req)
-
     res_body = res.read()
-
-    # https://docs.python.org/3/library/json.html
     j = json.loads(res_body.decode("utf-8"))
-    tRange.String = j['text']
 
+    # updating the selected text
+    oSel.setString(j['text'])
     return
