f478fe60f521df4d646d8d95b7b46556dfbe8f20
Kamil Kamaliev <gregoryfroydentaler23@gmail.com>
2020-11-29 06:19:46 +0300

    +high lighting

--- lo_macro.py
+++ lo_macro.py
@@ -5,26 +5,28 @@ import urllib.request
 import re
 from com.sun.star.drawing.TextHorizontalAdjust import LEFT
 
-
 def OneToThree(args=''):
+
     # preparation
     desktop = XSCRIPTCONTEXT.getDesktop()
     model = desktop.getCurrentComponent()
     oSelected = model.getCurrentSelection()
     doc = XSCRIPTCONTEXT.getDocument()
-    text = doc.getText()
+    startText = doc.getText()
+
 
     # acquiring the selected text
     oSel = oSelected.getByIndex(0)
-    selectedText = oSel.getString()
-    cursor = oSel.Text.createTextCursorByRange(oSel)
-
+    selectedText= oSel.getString()
+    
     # obtaining all text
-    allText = text.Text.String
+    allText = startText.Text.String
 
     # splitting the text into lines
     lines = allText.split('\n')
 
+    fname = ''
+   
     # obtaining full name
     for line in lines:
         if "Фамилия, имя, отчество" in line:
@@ -32,29 +34,41 @@ def OneToThree(args=''):
             fname = re.sub('[^а-яА-Я]+', ' ', fname)
             fname = ' '.join(fname.split(' '))
             break
-
-    # resolving issues with -
-    problems = re.findall('[а-яА-Я]+-\s[а-яА-Я]+', selectedText)
-    for word in problems:
-        upd = re.sub("-\s", '', word)
-        selectedText = selectedText.replace(word, upd)
+    
+    oSel.setString("")
 
     # sending the request to server
     toSend = {'fname': fname, "text": selectedText}
     data = json.dumps(toSend).encode("utf8")
     url = 'http://localhost:8080/send'
     req = urllib.request.Request(url, data=data, headers={'content-type': 'application/json'})
-
+    
     # transforming response to json
     res = urllib.request.urlopen(req)
     res_body = res.read()
     j = json.loads(res_body.decode("utf-8"))
 
     # change font size
-    cursor.setPropertyValue("CharHeight", 12)
-    cursor.setPropertyValue("CharFontName", "Times New Roman")
+    #cursor.setPropertyValue("CharHeight", 40)
 
     # updating the selected text
-    oSel.setString(j['text'])
-    # oSel.TextHorizontalAdjust = LEFT
+    #oSel.setString(j['text'])
+
+    cursor = oSel.Text.createTextCursorByRange(oSel)
+    resText = j['text']
+    changes = j['colored'] # [(3, 14), ]
+
+    curInd = 0
+    for start, end in changes:
+        if curInd <= start:
+            cursor.setPropertyValue('CharColor',0)
+            startText.insertString(cursor, resText[curInd:start], False)
+            cursor.setPropertyValue('CharColor',255)
+            startText.insertString(cursor, resText[start:end], False)
+            curInd = end
+
+    if curInd < len(resText):
+        cursor.setPropertyValue('CharColor',0)
+        startText.insertString(cursor, resText[curInd:], False)
+
     return
